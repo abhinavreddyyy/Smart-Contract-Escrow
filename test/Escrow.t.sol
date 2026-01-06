3339f03bc8ed945598eec4d0861330844825d0e2
abhinav <abhinavreddyyy@gmail.com>
2026-01-06 22:49:59 +0530

    Implemented few tersts for deployment, deposit

---
+++ test/Escrow.t.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Escrow} from "../src/Escrow.sol";
+
+contract EscrowTest is Test {
+    Escrow escrow; // The contract instance, to be initialized in setUp()
+
+    address buyer = address(1);
+    address seller = address(2);
+    uint256 amount = 1 ether;
+    uint256 timeout = 3 days;
+
+    function setUp() external {
+        vm.deal(buyer, 10 ether); // Fund the buyer with 10 ether
+        vm.deal(seller, 1 ether); // Fund the seller with 1 ether
+
+        vm.prank(buyer);
+        escrow = new Escrow(seller, amount);
+    }
+
+    /** DEPLOYMENT */
+    function testInitialState() external {
+        assertEq(
+            uint256(escrow.currentStatus()),
+            uint256(Escrow.Status.AWAITING_PAYMENT)
+        );
+        assertEq(escrow.buyer(), buyer);
+        assertEq(escrow.seller(), seller);
+        assertEq(escrow.amount(), amount);
+    }
+
+    /** DEPOSIT */
+    function testDepositFailsIfNotBuyer() external {
+        //vm.prank(seller);
+        vm.expectRevert(Escrow.NotBuyer.selector);
+        escrow.deposit{value: amount}();
+    }
+
+    function testDepositFailsInWrongState() external {
+        vm.prank(buyer);
+        escrow.deposit{value: amount}();
+
+        // Now the state is AWAITING_DELIVERY
+        vm.prank(buyer);
+        vm.expectRevert(Escrow.InvalidState.selector);
+        escrow.deposit{value: amount}();
+    }
+
+    function testDepositFailsWithWrongETHAmount() external {
+        vm.prank(buyer);
+        vm.expectRevert(Escrow.IncorrectETHAmount.selector);
+        escrow.deposit{value: amount - 1 ether}();
+    }
+
+    function testIfDeadlineIsSetAfterDeposit() external {
+        vm.prank(buyer);
+        escrow.deposit{value: amount}();
+
+        uint256 expectedDeadline = block.timestamp + 3 days;
+        assertEq(escrow.deadline(), expectedDeadline);
+    }
+
+    function testSuccessfulDeposit() external {
+        vm.prank(buyer);
+        escrow.deposit{value: amount}();
+        assertEq(
+            uint256(escrow.currentStatus()),
+            uint256(Escrow.Status.AWAITING_DELIVERY)
+        );
+    }
+}
